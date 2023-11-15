f5e1c0429c61e7081592264399f602d967729217
MartinTarigan <martin.marcelino@ui.ac.id>
2023-11-15 16:39:00 +0700

    submit tutorial 8

--- lib/widgets/shop_card.dart
+++ lib/widgets/shop_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/screens/list_product.dart';
 import 'package:shopping_list/screens/shoplist_form.dart';
 
 class ShopItem {
@@ -28,6 +29,11 @@ class ShopCard extends StatelessWidget {
           if (item.name == "Tambah Produk") {
             Navigator.push(context, MaterialPageRoute(builder: (context) => const ShopFormPage()));
           }
+          else if (item.name == "Lihat Produk") {
+            Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const ProductPage()));
+          }
+
         },
         child: Container(
           // Container untuk menyimpan Icon dan Text
